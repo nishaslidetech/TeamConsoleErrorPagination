7cf201b336b0acda48874012512c8ac09c7ea77d
nishaslidetech <nisha.dhiman@slidetech.in>
2023-03-03 15:08:33 +0530

    first commit

--- src/test/java/stepDefination/MostDownload.java
+++ src/test/java/stepDefination/MostDownload.java
@@ -134,76 +134,67 @@ public class MostDownload extends BaseClass {
 
 	@Then("^click on the Filter By Category and check the console error$")
 	public void click_on_the_Filter_By_Category_and_check_the_console_error() throws Throwable {
-		try {
-			WebElement filterByCategory = wait.until(ExpectedConditions
-					.elementToBeClickable(By.xpath("//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
-			js.executeScript("arguments[0].scrollIntoView();", filterByCategory);
-
-			js.executeScript("arguments[0].click();", filterByCategory);
-			Thread.sleep(2000);
-			// check console error for the 120 pages
-			List<WebElement> diagrams = driver.findElements(By.xpath("//div[@class = 'choices__list']//div[2]"));
-
-			diagrams.get(0).click();
-			Thread.sleep(2000);
-			// print console error
-			checkConsoleError();
-
-			Thread.sleep(2000);
-
-			filterByCategory = wait.until(ExpectedConditions
-					.elementToBeClickable(By.xpath("//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
-
-			js.executeScript("arguments[0].click();", filterByCategory);
-			Thread.sleep(2000);
-
-			// check console error for the 180 pages
-			chatWindow();
-			Thread.sleep(2000);
-			List<WebElement> flatDesigns = driver.findElements(By.xpath("//div[@class = 'choices__list']//div[3]"));
-			// js.executeScript("arguments[0].scrollIntoView();", flatDesigns);
-			flatDesigns.get(0).click();
-			Thread.sleep(2000);
-			// print console error
-			checkConsoleError();
-
-			Thread.sleep(2000);
-			filterByCategory = wait.until(ExpectedConditions
-					.elementToBeClickable(By.xpath("//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
-
-			js.executeScript("arguments[0].scrollIntoView();", filterByCategory);
-			js.executeScript("arguments[0].click();", filterByCategory);
-			Thread.sleep(2000);
-
-			Thread.sleep(2000);
-			List<WebElement> businessSlides = driver.findElements(By.xpath("//div[@class = 'choices__list']//div[4]"));
-			// js.executeScript("arguments[0].scrollIntoView();", businessSlides);
-			businessSlides.get(0).click();
-			Thread.sleep(2000);
-			// print console error
-			checkConsoleError();
-
-			Thread.sleep(2000);
-			filterByCategory = wait.until(ExpectedConditions
-					.elementToBeClickable(By.xpath("//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
-
-			js.executeScript("arguments[0].click();", filterByCategory);
-			Thread.sleep(2000);
-
-			Thread.sleep(2000);
-			List<WebElement> completeDesk = driver.findElements(By.xpath("//div[@class = 'choices__list']//div[5]"));
-			// js.executeScript("arguments[0].scrollIntoView();", completeDesk);
-			completeDesk.get(0).click();
-			Thread.sleep(2000);
-
-			checkConsoleError();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (WebDriverManagerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		/*
+		 * try { WebElement filterByCategory = wait.until(ExpectedConditions
+		 * .elementToBeClickable(By.xpath(
+		 * "//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
+		 * js.executeScript("arguments[0].scrollIntoView();", filterByCategory);
+		 * 
+		 * js.executeScript("arguments[0].click();", filterByCategory);
+		 * Thread.sleep(2000); // check console error for the 120 pages List<WebElement>
+		 * diagrams =
+		 * driver.findElements(By.xpath("//div[@class = 'choices__list']//div[2]"));
+		 * 
+		 * diagrams.get(0).click(); Thread.sleep(2000); // print console error
+		 * checkConsoleError();
+		 * 
+		 * Thread.sleep(2000);
+		 * 
+		 * filterByCategory = wait.until(ExpectedConditions
+		 * .elementToBeClickable(By.xpath(
+		 * "//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
+		 * 
+		 * js.executeScript("arguments[0].click();", filterByCategory);
+		 * Thread.sleep(2000);
+		 * 
+		 * // check console error for the 180 pages chatWindow(); Thread.sleep(2000);
+		 * List<WebElement> flatDesigns =
+		 * driver.findElements(By.xpath("//div[@class = 'choices__list']//div[3]")); //
+		 * js.executeScript("arguments[0].scrollIntoView();", flatDesigns);
+		 * flatDesigns.get(0).click(); Thread.sleep(2000); // print console error
+		 * checkConsoleError();
+		 * 
+		 * Thread.sleep(2000); filterByCategory = wait.until(ExpectedConditions
+		 * .elementToBeClickable(By.xpath(
+		 * "//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
+		 * 
+		 * js.executeScript("arguments[0].scrollIntoView();", filterByCategory);
+		 * js.executeScript("arguments[0].click();", filterByCategory);
+		 * Thread.sleep(2000);
+		 * 
+		 * Thread.sleep(2000); List<WebElement> businessSlides =
+		 * driver.findElements(By.xpath("//div[@class = 'choices__list']//div[4]")); //
+		 * js.executeScript("arguments[0].scrollIntoView();", businessSlides);
+		 * businessSlides.get(0).click(); Thread.sleep(2000); // print console error
+		 * checkConsoleError();
+		 * 
+		 * Thread.sleep(2000); filterByCategory = wait.until(ExpectedConditions
+		 * .elementToBeClickable(By.xpath(
+		 * "//div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]")));
+		 * 
+		 * js.executeScript("arguments[0].click();", filterByCategory);
+		 * Thread.sleep(2000);
+		 * 
+		 * Thread.sleep(2000); List<WebElement> completeDesk =
+		 * driver.findElements(By.xpath("//div[@class = 'choices__list']//div[5]")); //
+		 * js.executeScript("arguments[0].scrollIntoView();", completeDesk);
+		 * completeDesk.get(0).click(); Thread.sleep(2000);
+		 * 
+		 * checkConsoleError(); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); } catch
+		 * (WebDriverManagerException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 */
 
 	}
 }
